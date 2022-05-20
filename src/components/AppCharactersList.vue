3999e98b02d4e03e82eb22c4f1c3fda1f4e605e3
olga_demina <olga.demina1410@gmail.com>
2022-05-20 13:00:18 +0200

    Aggiunta la ricerca

--- src/components/AppCharactersList.vue
+++ src/components/AppCharactersList.vue
@@ -1,5 +1,9 @@
 <template>
   <section>
+    <AppSearch
+      @searchClicked="saveSearchKey($event)"
+      @resetClicked="resetSearchKey"
+    />
     <div class="main-content">
       <div v-if="loading" class="container">
         <AppLoading />
@@ -7,13 +11,13 @@
       <div v-else class="container">
         <div class="row row-cols-2 row-cols-md-4">
           <AppCharacterCard
-            v-for="item in characters"
+            v-for="item in filterCharacters"
             :key="item.id"
             :character="item"
           />
         </div>
         <div class="row">
-          <AppCount :charactersNumber="characters.length"/>
+          <AppCount :charactersNumber="filterCharacters.length" />
         </div>
       </div>
     </div>
@@ -24,6 +28,7 @@
 import AppCharacterCard from "./AppCharacterCard.vue";
 import AppLoading from "./AppLoading.vue";
 import AppCount from "./AppCount.vue";
+import AppSearch from "./AppSearch.vue";
 import axios from "axios";
 
 export default {
@@ -32,11 +37,13 @@ export default {
     AppCharacterCard,
     AppLoading,
     AppCount,
+    AppSearch,
   },
   data: function () {
     return {
       characters: [],
       loading: true,
+      search: "",
     };
   },
   created() {
@@ -46,6 +53,23 @@ export default {
         this.characters = resp.data;
         this.loading = false;
       });
+  },
+  computed: {
+    filterCharacters: function () {
+      const keyFormatted = this.search.toLowerCase();
+      const filteredCharacters = this.characters.filter((item) => {
+        return item.name.toLowerCase().includes(keyFormatted);
+      });
+      return filteredCharacters;
+    },
+  },
+  methods: {
+    saveSearchKey: function (searchKey) {
+      this.search = searchKey;
+    },
+    resetSearchKey() {
+      this.search = "";
+    },
   },
 };
 </script>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="search container d-flex">
+    <div class="form-group flex-grow-1 me-2">
+      <input
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Inizia la ricerca"
+        v-model="searchWord"
+      />
+    </div>
+    <button class="btn btn-primary me-2" @click="$emit('searchClicked', searchWord)">
+      Cerca
+    </button>
+     <!-- <button class="btn btn-primary me-2" @click="sendEvent()">
+      Cerca
+    </button> -->
+    <button class="btn btn-warning" @click="resetSearch">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSearch",
+  data: function () {
+    return {
+      searchWord: "",
+    };
+  },
+  methods: {
+    // sendEvent() {
+    //   // Fare varie operazioni
+    //   this.$emit("searchClicked", this.searchWord);
+    // }
+    resetSearch: function() {
+      this.searchWord = "";
+      this.$emit("resetClicked");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
